6e937edb6aab7518a77bf34748010a30c51a201a
Francois Cassin <francois.cassin@gmail.com>
2013-05-23 10:44:09 +0200

    Gestion differente du login et mot de passe, gere le mode sans identification

--- app/controllers/WSHelper.scala
+++ app/controllers/WSHelper.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api.libs.json._
 import play.api.libs.ws._
+import play.api.libs.ws.WS
+import play.api.libs.ws.WS.WSRequestHolder
 
 import play.api.mvc.Action
 import play.api.mvc.Controller
@@ -16,11 +18,19 @@ import ExecutionContext.Implicits.global
 
 object WSHelper {
 
-  def login = "fcassin";
-  def password = "retycui0";
+  def login = "";
+  def password = "";
+
+  def getWSCaller(url: String): WSRequestHolder = {
+    if (login != "" && password != "") {
+      WS.url(url).withAuth(login, password, com.ning.http.client.Realm.AuthScheme.BASIC)  
+    } else {
+      WS.url(url)
+    }
+  }
 
 	def getRepositories(url: String): Future[JsArray] = {
-  		val repositories: Future[Response] = WS.url(url).withAuth(login, password, com.ning.http.client.Realm.AuthScheme.BASIC).get()
+  		val repositories: Future[Response] = getWSCaller(url).get()
   		repositories.map { response => 
 			val responseJson = response.json.as[JsObject]
 			(responseJson \ "repositories").as[JsArray]
@@ -28,14 +38,14 @@ object WSHelper {
   	}
 
   	def getJsObject(url: String): Future[JsObject] = {
-  		val repository: Future[Response] = WS.url(url).withAuth(login, password, com.ning.http.client.Realm.AuthScheme.BASIC).get()
+  		val repository: Future[Response] = getWSCaller(url).get()
   		repository.map { response => 
 			response.json.as[JsObject]
   		}
   	}
 
   	def getJsArray(url: String): Future[JsArray] = {
-  		val repository: Future[Response] = WS.url(url).withAuth(login, password, com.ning.http.client.Realm.AuthScheme.BASIC).get()
+  		val repository: Future[Response] = getWSCaller(url).get()
   		repository.map { response => 
 			response.json.as[JsArray]
   		}
